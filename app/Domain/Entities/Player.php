a43a24518fd9db19835fc8302dd9d4afa93deb4e
Guilherme-07062002 <guilherme762002@gmail.com>
2023-08-13 00:21:31 -0300

    Criação entidade Player

---
+++ app/Domain/Entities/Player.php
@@ -0,0 +1,13 @@
+<?php
+
+class Player
+{
+    public $name;
+    public $email;
+
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+}
