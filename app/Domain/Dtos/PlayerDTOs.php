01f4eea00938c5cc2af4f6a87fd113f2aa9feb55
Guilherme-07062002 <guilherme762002@gmail.com>
2023-08-13 00:38:06 -0300

    CreatePlayerDTO

---
+++ app/Domain/Dtos/PlayerDTOs.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Domain\DTOs\PlayerDTOs;
+
+class CreatePlayerDTO
+{
+    public $name;
+    public $email;
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+}
